448772e77d8ac798690f6954743666a192795500
Sinclair Target <sinclairtarget@gmail.com>
2017-09-06 14:28:40 -0400

    Set up basic AJAX request.

--- application.js
+++ application.js
@@ -1,1 +1,29 @@
-console.log("Hello!")
+showLoading = function() {
+    $(".loading-indicator").show();
+    $(".results").hide();
+}
+
+showResults = function(periods) {
+    $(".loading-indicator").hide();
+    $(".results").show();
+    console.log(periods);
+}
+
+fetchWeather = function() {
+    url = "http://api.aerisapi.com/forecasts/11101?client_id=7YerZwAFYOYbaKl59822u&client_secret=qgBnkFBZbBC4LtTs5qtuM46G6khNfH6e7QODCFyD"
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).done(function(result) {
+        if (result["success"] != true)
+            console.log(result["error"]);
+
+        showResults(result["response"][0]["periods"]);
+    }).fail(function(err) {
+        throw err;
+    });
+}
+
+$(function() {
+    fetchWeather();
+});
